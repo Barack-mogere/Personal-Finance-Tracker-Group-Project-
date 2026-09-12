07ad6bbfef01967e0aa3b1b5ba1532adde528e9e
EmmanuelMwangi021 <manumwangi2@gmail.com>
2026-09-13 00:57:26 +0300

    Finished upon expense class

--- models/expense.py
+++ models/expense.py
@@ -1,1 +1,10 @@
 # emmanuel
+from models.transaction import Transaction
+
+
+class Expense(Transaction):
+    def __init__(self, amount, category, description, date, user_id):
+        super().__init__(amount, category, description, date, user_id)
+
+    def __str__(self):
+        return f"Expense: {self.category} - Ksh {self.amount} - {self.description}"
