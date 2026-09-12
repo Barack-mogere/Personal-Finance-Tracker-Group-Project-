40760aa24068733a3b31df05f208dd649af57479
EmmanuelMwangi021 <manumwangi2@gmail.com>
2026-09-12 17:03:11 +0300

    Created user class

--- models/user.py
+++ models/user.py
@@ -1,1 +1,25 @@
 # emmanuel
+from models.person import Person
+
+class User(Person):
+    def __init__(self, name, email, password, role="user"):
+        super().__init__(name)
+
+        self._email = email
+        self._password = password
+        self._role = role
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def role(self):
+        return self._role
+
+    def __str__(self):
+        return f"{self.name} - {self.email} - {self.role}"
