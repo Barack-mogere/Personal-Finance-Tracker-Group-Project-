3c07cbd99f7ecd34b886d05687f19c3e9bac65f7
barack <barackannan00@gmail.com>
2026-09-13 08:47:28 +0300

    Add user and admin instances to main.py; update transaction.py header

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Barack
+from models.user import User
+
+
+user = User(
+    "Emmanuel",
+    "emmanuel@gmail.com",
+    "password123"
+)
+
+admin = User(
+    "Barack",
+    "barack@gmail.com",
+    "adminpassword",
+    "admin"
+)
+
+print(user)
+print(admin)
