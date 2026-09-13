a51ed51ca3022cdd1049933c0e385b2e366afcaa
beatrice <beatrice.kamoni@student.moringaschool.com>
2026-09-13 14:43:40 +0300

    decorators

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,1 +1,30 @@
 # Beatrice
+from functools import wraps
+
+
+def login_required(func):
+    @wraps(func)
+    def wrapper(user, *args, **kwargs):
+        if user is None:
+            print("Please log in first.")
+            return None
+
+        return func(user, *args, **kwargs)
+
+    return wrapper
+
+
+def admin_required(func):
+    @wraps(func)
+    def wrapper(user, *args, **kwargs):
+        if user is None:
+            print("Please log in first.")
+            return None
+
+        if user.role != "admin":
+            print("Admin access required.")
+            return None
+
+        return func(user, *args, **kwargs)
+
+    return wrapper
