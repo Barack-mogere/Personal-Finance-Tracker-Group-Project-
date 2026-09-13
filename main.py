55675195a5f4a7902d6f9fec5d1f8da10cabe61b
barack <barackannan00@gmail.com>
2026-09-13 08:55:18 +0300

    Remove user and admin instances from main.py; add compiled bytecode for expense, income, and transaction models

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
 # Barack
-from models.user import User
-
-
-user = User(
-    "Emmanuel",
-    "emmanuel@gmail.com",
-    "password123"
-)
-
-admin = User(
-    "Barack",
-    "barack@gmail.com",
-    "adminpassword",
-    "admin"
-)
-
-print(user)
-print(admin)
