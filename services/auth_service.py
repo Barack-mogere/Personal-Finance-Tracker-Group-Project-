0eaaae0c772abc1a10b8ad7f84008f07f6a83250
beatrice <beatrice.kamoni@student.moringaschool.com>
2026-09-13 14:38:37 +0300

    AuthService

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,1 +1,13 @@
 # Beatrice
+import hashlib
+
+
+class AuthService:
+
+    @staticmethod
+    def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    @staticmethod
+    def verify_password(password, hashed_password):
+        return AuthService.hash_password(password) == hashed_password
