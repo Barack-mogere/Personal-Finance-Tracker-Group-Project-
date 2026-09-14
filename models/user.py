45506abe3a7115637202e783d2b38495fc60fa5b
barack <barackannan00@gmail.com>
2026-09-14 09:55:20 +0300

    Add user ID to User class

--- models/user.py
+++ models/user.py
@@ -1,13 +1,15 @@
 # emmanuel
 from models.person import Person
 
+
 class User(Person):
-    def __init__(self, name, email, password, role="user"):
+    def __init__(self, name, email, password, role="user", user_id=None):
         super().__init__(name)
 
         self._email = email
         self._password = password
         self._role = role
+        self._user_id = user_id
 
     @property
     def email(self):
@@ -20,6 +22,10 @@ class User(Person):
     @property
     def role(self):
         return self._role
+
+    @property
+    def user_id(self):
+        return self._user_id
 
     def __str__(self):
         return f"{self.name} - {self.email} - {self.role}"
